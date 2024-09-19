449e82a2c465015b38b389abedc095842e07d2a0
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2024-09-19 20:17:03 +0200

    refactor(config): use environment variables for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,22 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Person } from './entities/person.entity';
 import { Animal } from './entities/animal.entity';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'adminAnimalis',
-      password: 'WJAkzemDkxmy2sC',
-      database: 'animalis',
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Person, Animal],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Person, Animal]),
   ],
   controllers: [AppController],
   providers: [AppService],
