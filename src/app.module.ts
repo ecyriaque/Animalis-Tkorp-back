6b835da435d25e708e8b162e0fa32fc27acc8e47
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2024-09-19 13:29:42 +0200

    feat(database): add initial setup for MySQL database and entities
    - Configure TypeORM for MySQL in `app.module.ts`.
    - Create `Person` and `Animal` entities with relationships.
    - Define one-to-many relationship between `Person` and `Animal`.
    - Define many-to-one relationship from `Animal` to `Person`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person.entity';
+import { Animal } from './entities/animal.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'adminAnimalis',
+      password: 'WJAkzemDkxmy2sC',
+      database: 'animalis',
+      entities: [Person, Animal],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Person, Animal]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
