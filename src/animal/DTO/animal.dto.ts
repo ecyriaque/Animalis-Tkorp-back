b95bc618367e23fd972c8f882f9e2c0dd10fbcd0
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2024-09-20 12:02:44 +0200

    feat(dto): add Animal DTO for validation and data transfer
    - Created AnimalDto to handle validation for animal data
    
    - Includes fields for name, dateOfBirth, species, breed, color, weight, and ownerId

---
+++ src/animal/DTO/animal.dto.ts
@@ -0,0 +1,26 @@
+import { IsString, IsDate, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class AnimalDto {
+  @IsString()
+  name: string;
+
+  @IsDate()
+  @Type(() => Date)
+  dateOfBirth: Date;
+
+  @IsString()
+  species: string;
+
+  @IsString()
+  breed: string;
+
+  @IsString()
+  color: string;
+
+  @IsNumber()
+  weight: number;
+
+  @IsNumber()
+  ownerId: number; // Assure-toi d'ajouter l'ID du propriétaire si nécessaire
+}
